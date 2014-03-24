e74dab5e4e5f3f4d386efdf16f1f618cb7b7af0e
Wilson Y. Wong <Wilson.Young.Wong@gmail.com>
2014-03-24 12:57:57 -0700

    sql to set up tables

--- Resources/createTables.sql
+++ Resources/createTables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS BorrowerType (
-  type varchar(10),
+  type varchar(7),
   bookTimeLimit varchar(10),
   PRIMARY KEY (type)
 );
@@ -10,11 +10,69 @@ CREATE TABLE IF NOT EXISTS Borrower (
   name varchar(40) NOT NULL,
   address varchar(40),
   phone char(10),
-  emailAddress varchar(40),
-  sinOrStNo varchar(20) NOT NULL,
+  emailAddress varchar(40) NOT NULL,
+  sinOrStNo varchar(9) NOT NULL,
   expiryDate varchar(10),
-  type varchar(10),
+  type varchar(7),
   PRIMARY KEY (bid),
   FOREIGN KEY (type) REFERENCES BorrowerType(type)
 );
 
+CREATE TABLE IF NOT EXISTS Book (
+  callNumber varchar(40) NOT NULL,
+  isbn varchar(40) NOT NULL,
+  title varchar(40) NOT NULL,
+  mainAuthor varchar(40) NOT NULL,
+  publisher varchar(40) NOT NULL,
+  year int NOT NULL,
+  PRIMARY KEY (callNumber)
+);
+
+CREATE TABLE IF NOT EXISTS HasAuthor (
+  callNumber varchar(40) NOT NULL,
+  name varchar(40) NOT NULL,
+  PRIMARY KEY (callNumber, name),
+  FOREIGN KEY (callNumber) REFERENCES Book(callNumber)
+);
+
+CREATE TABLE IF NOT EXISTS HasSubject (
+  callNumber varchar(40) NOT NULL,
+  subject varchar(40) NOT NULL,
+  PRIMARY KEY (callNumber, subject),
+  FOREIGN KEY (callNumber) REFERENCES Book(callNumber)
+);
+
+CREATE TABLE IF NOT EXISTS BookCopy (
+  callNumber varchar(40) NOT NULL,
+  copyNo int NOT NULL,
+  status varchar(7) NOT NULL,
+  PRIMARY KEY (callNumber, copyNo),
+  FOREIGN KEY (callNumber) REFERENCES Book(callNumber)
+);
+
+CREATE TABLE IF NOT EXISTS HoldRequest (
+  hid varchar(40) NOT NULL,
+  bid char(8) NOT NULL,
+  callNumber varchar(40) NOT NULL,
+  issuedDate DATE NOT NULL,
+  PRIMARY KEY (hid)
+);
+
+CREATE TABLE IF NOT EXISTS Borrowing (
+  borid varchar(40) NOT NULL,
+  bid char(8) NOT NULL,
+  callNumber varchar(40) NOT NULL,
+  copyNo int NOT NULL,
+  outDate DATE NOT NULL,
+  inDate DATE NOT NULL,
+  PRIMARY KEY (borid)
+);
+
+CREATE TABLE IF NOT EXISTS Fine (
+  fid varchar(40) NOT NULL,
+  amount int NOT NULL,
+  issuedDate DATE NOT NULL,
+  paidDate DATE NOT NULL,
+  borid varchar(40) NOT NULL,
+  PRIMARY KEY (fid)
+);
