8301c033a662f9c30c29df7f8d1eb4f3d03f67c1
Wilson Y. Wong <Wilson.Young.Wong@gmail.com>
2014-03-25 16:15:44 -0700

    simple login screen

--- site.py
+++ site.py
@@ -1,9 +1,29 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
 app = Flask(__name__)
 
 @app.route("/")
+def welcome():
+    return render_template('welcome.html')
+
+@app.route("/index")
 def index(name=None):
     return render_template('index.html', name=name)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
 if __name__ == '__main__':
     app.run()
