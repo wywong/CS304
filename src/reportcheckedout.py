7b529d0d86c17b6cbcbefd58ce5cab5c57e62cf0
Evan Friday <evan_friday@hotmail.com>
2014-04-01 01:29:49 -0700

    fixing reportcheckedout subject filter

--- src/reportcheckedout.py
+++ src/reportcheckedout.py
@@ -21,24 +21,21 @@ def reportcheckedout(subject=None):
         _subject = ""
 
         dates = TableOperation.sfw('Borrowing AS bor INNER JOIN Borrower AS b ON (bor.bid = b.bid) INNER JOIN BorrowerType AS bt ON (b.type = bt.type)',['outDate','bookTimeLimit','bor.borid'],"bor.inDate= '0000-00-00'")
-        for ii in dates:
-            print ii
         duedates = [r[0]+datetime.timedelta(r[1]*7) for r in dates]
         today = date.today()
         overdue = [today > d for d in duedates]
         rows = TableOperation.sfw('Borrowing AS bor INNER JOIN Borrower AS b ON (bor.bid = b.bid) INNER JOIN Book AS bk ON (bor.callNumber = bk.callNumber) INNER JOIN HasSubject AS h ON (bk.callNumber = h.callNUmber) INNER JOIN BookCopy AS bc ON (bc.callNumber=bk.callNumber)', ['bk.callNumber','bk.isbn','bk.title','bk.mainAuthor','h.subject','bc.copyNo','b.name','bor.outDate'],"bor.inDate = '0000-00-00' and bor.copyNo=bc.copyNo")
-        for ii in rows:
-            print ii
         rows = [[x if type(x) is not date else str(x) for x in y] for y in rows]
         [x.append(duedates.pop(0).isoformat())for x in rows]
     else:
+        print(_subject)
         _subject = _subject.encode('utf-8')
         dates = TableOperation.getColumns('Borrowing AS bor INNER JOIN Borrower AS b ON (bor.bid = b.bid) INNER JOIN BorrowerType AS bt ON (b.type = bt.type)',['outDate','bookTimeLimit'])
         duedates = [r[0]+datetime.timedelta(r[1]*7) for r in dates]
         today = date.today()
         overdue = [today > d for d in duedates]
         rows = TableOperation.sfw('Borrowing AS bor INNER JOIN Borrower AS b ON (bor.bid = b.bid) INNER JOIN Book AS bk ON (bor.callNumber = bk.callNumber) INNER JOIN HasSubject AS h ON (bk.callNumber = h.callNUmber) INNER JOIN BookCopy AS bc ON (bc.callNumber=bk.callNumber)', ['bk.callNumber','bk.isbn','bk.title','bk.mainAuthor','h.subject','bc.copyNo','b.name','bor.outDate'],
-            "h.subject = '%%%s%%' and bor.inDate = '0000-00-00' and bor.copyNo=bc.copyNo" % _subject)
+            "h.subject LIKE '%%%s%%' and bor.inDate = '0000-00-00' and bor.copyNo=bc.copyNo" % _subject)
         rows = [[x if type(x) is not date else str(x) for x in y] for y in rows]
         [x.append(duedates.pop(0).isoformat())for x in rows]
 
