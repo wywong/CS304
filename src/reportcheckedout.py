18b6dce30c0b541d2db9817e2e62e6457fa32a67
EvanFriday <Evan_Friday@hotmail.com>
2014-03-31 17:20:05 -0700

    loading checked out books

--- src/reportcheckedout.py
+++ src/reportcheckedout.py
@@ -18,14 +18,12 @@ def reportcheckedout(subject=None):
         _subject = ""
         
         dates = TableOperation.getColumns('Borrowing AS bor INNER JOIN Borrower AS b ON (bor.bid = b.bid) INNER JOIN BorrowerType AS bt ON (b.type = bt.type)',['bookTimeLimit','outDate'])
-        duedates = ""
         rows = TableOperation.sfw('Borrowing AS bor INNER JOIN Borrower AS b ON (bor.bid = b.bid) INNER JOIN Book AS bk ON (bor.callNumber = bk.callNumber) INNER JOIN HasSubject AS h ON (bk.callNumber = h.callNUmber) INNER JOIN BookCopy AS bc ON (bc.callNumber=bk.callNumber)', ['bk.callNumber','bk.isbn','bk.title','bk.mainAuthor','h.subject','bc.copyNo','b.name','bor.outDate'],"bor.inDate = '0000-00-00'")
     else:
         _subject = _subject.encode('utf-8')
         dates = TableOperation.getColumns('Borrowing AS bor INNER JOIN Borrower AS b ON (bor.bid = b.bid) INNER JOIN BorrowerType AS bt ON (b.type = bt.type)',['bookTimeLimit','outDate'])
-        duedates = ""
         rows = TableOperation.sfw('Borrowing AS bor INNER JOIN Borrower AS b ON (bor.bid = b.bid) INNER JOIN Book AS bk ON (bor.callNumber = bk.callNumber) INNER JOIN HasSubject AS h ON (bk.callNumber = h.callNUmber) INNER JOIN BookCopy AS bc ON (bc.callNumber=bk.callNumber)', ['bk.callNumber','bk.isbn','bk.title','bk.mainAuthor','h.subject','bc.copyNo','b.name','bor.outDate'],
             "h.subject = '%%%s%%' and bor.inDate = '0000-00-00'" % _subject)
     
-    
+    session['reportcheckedout'] = [rows]
     return render_template('reportcheckedout.html', user=g.userInfo[0], accType=g.userInfo[8])
